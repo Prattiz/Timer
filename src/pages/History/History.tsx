84945d86acc00233b114c1d5d2be4414fb6f56b3
Thiago <prattithiago5@gmail.com>
2023-10-06 17:25:38 -0300

    UPDATE CSS HISTORY

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,9 +1,31 @@
+import { Container, HistoryList } from "./history.styles";
 
 
 export function History(){
     return(
-        <div>
-            <h1>history</h1>
-        </div>
+        <Container>
+            <h1>My Historic</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tasks</th>
+                            <th>Duration</th>
+                            <th>Start</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>T1</td>
+                            <td>d1</td>
+                            <td>s1</td>
+                            <td>STA1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </Container>
     )
 }
--- src/pages/History/history.styles.ts
+++ src/pages/History/history.styles.ts
@@ -1,3 +1,64 @@
 import styled from "styled-components";
 
-export const Container = styled.main``
+export const Container = styled.main`
+
+    flex: 1;
+    padding: 4rem;
+    display: flex;
+    flex-direction: column;
+
+    h1{
+        font-size: 2rem;
+        color: ${props => props.theme["gray-100"]};
+    }
+`
+
+export const HistoryList = styled.div`
+
+    flex:1;
+    overflow: auto;
+    margin-top: 2rem;
+
+    table{
+        width: 100%;
+        border-collapse: collapse;
+        min-width: 600px;
+
+        th{
+            background-color: ${props => props.theme["gray-600"]};
+            color:${props => props.theme["gray-100"]};
+            padding: 1.6rem;
+            font-size: 1.4rem;
+            line-height: 1.6;
+            text-align: left;
+
+            &:first-child{
+                border-top-left-radius: 8px;
+                padding-left: 2rem;
+            }
+            &:last-child{
+                border-top-right-radius: 8px; 
+                padding-right: 2rem;   
+            }}
+        
+        td{
+            background-color: ${props => props.theme["gray-700"]};
+            border-top: 4px solid ${props => props.theme["gray-800"]};
+            padding: 1.6rem;
+            font-size: 1.4rem;
+            line-height: 1.6;
+
+            &:first-child{
+                border-bottom-left-radius: 8px;
+                padding-left: 2rem;
+            }
+            &:last-child{
+                border-bottom-right-radius: 8px; 
+                padding-right: 2rem;   
+            }
+
+        }
+
+    
+    }
+`
